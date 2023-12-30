7370305b60a1c8326e7e1bd824de4251dd233427
Lee Thompson <lee.p.thomp@gmail.com>
2023-12-30 23:37:21 +0000

    Quick copy verbatim from book.

---
+++ rkt/2/21.rkt
@@ -0,0 +1,14 @@
+;;; -*- mode:scheme; indent-tabs-mode:nil; coding:utf-8 -*-
+#lang racket
+
+;;;; Exercise 2.21: (Pg. 144)
+
+(require test-engine/racket-tests)
+
+(define (square-list items)
+  (if (null? items)
+      nil
+      (cons 〈??〉 〈??〉)))
+
+(define (square-list’ items)
+  (map 〈??〉 〈??〉))

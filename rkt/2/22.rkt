fec74d40cacb4316a5160f1b59d3eb4f57557417
Lee Thompson <lee.p.thomp@gmail.com>
2024-01-01 19:16:32 +0000

    Comment explaining one part of the problem.

---
+++ rkt/2/22.rkt
@@ -0,0 +1,7 @@
+;;; -*- mode:scheme; indent-tabs-mode:nil; coding:utf-8 -*-
+#lang racket
+
+;;;; Exercise 2.22: (Pg. 145)
+
+;;; Louis’ procedure first conses `(square (car things))' onto an empty list,
+;;; then conses onto the front of this list, creating a backwards squared list.

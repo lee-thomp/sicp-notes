d9e149a5505ef7ac0c9279ca577db673f762efb9
Lee Thompson <lee@opteran.com>
2025-09-29 15:34:22 +0100

    Moved to Org mode code blocks

---
+++ guile/.dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode . ((eval . (org-babel-do-load-languages
+                       'org-babel-load-languages
+                       '((scheme . t)))))))
